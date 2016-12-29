747dd6645ddfab6f0f41d31ef3e0ca32a4bec147
Benjamin Müller <bm@nexplore.ch>
2016-12-29 15:52:47 +0100

    Add some nodes

--- Source/FirstPlot.m
+++ Source/FirstPlot.m
@@ -4,7 +4,11 @@ bern = Model.Node('Bern', 1);
 scho = Model.Node('Schönbühl', 2);
 biel = Model.Node('Biel', 3);
 lute = Model.Node('Luterbach', 4);
-zuer = Model.Node('zürich', 5);
+haer = Model.Node('Härkingen', 5);
+oftr = Model.Node('Oftringen', 6);
+basl = Model.Node('Basel', 7);
+
+zuer = Model.Node('Zürich', 5);
 
 bern.connect(Model.Lane(100, 6.1, 0), scho);
 scho.connect(Model.Lane(100, 6.1, 10), bern);
@@ -14,12 +18,21 @@ biel.connect(Model.Lane(80, 17.3, 0), lute);
 lute.connect(Model.Lane(80, 17.3, 0), biel);
 scho.connect(Model.Lane(120, 24.5, 0), lute);
 lute.connect(Model.Lane(120, 24.5, 0), scho);
+lute.connect(Model.Lane(120, 19.6, 0), haer);
+haer.connect(Model.Lane(120, 19.6, 0), lute);
+haer.connect(Model.Lane(120, 7.9, 8), oftr);
+oftr.connect(Model.Lane(120, 7.9, 2), haer);
+haer.connect(Model.Lane(100, 35.9, 0), basl);
+basl.connect(Model.Lane(100, 35.9, 0), haer);
 
 st = [];
 st = x(st, bern);
 st = x(st, scho);
 st = x(st, biel);
 st = x(st, lute);
+st = x(st, haer);
+st = x(st, oftr);
+st = x(st, basl);
 
 G = digraph(st(:,1), st(:,2), st(:,3));
 plot(G, 'EdgeLabel', st(:,3));
